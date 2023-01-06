fd21be4aa5092848947a2089df21d74bd0e66e23
Dmitry Averyanov <averyanov-22@yandex.ru>
2023-01-06 20:59:40 +0300

    удален закомментрированный ненужный код в main

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,7 @@ import Tasks.Utils.Status;
 
 public class Main {
     public static void main(String[] args) {
-//        ManagerTask managerTask = new ManagerTask();
-//        ManagerEpic managerEpic = new ManagerEpic();
-//        ManagerSubtask managerSubtask =
         Manager manager = new Manager();
-//
-//        SubTask subTask1 = new SubTask("Подзадача-1", "Описание подзадачи-1");
-//        SubTask subTask2 = new SubTask("Подзадача-2", "Описание подзадачи-3");
-//        SubTask subTask3 = new SubTask("Подзадача-3", "Описание подзадачи-2");
-//
-//        Epic epic1 = manager.createEpic("Эпичная задача-1", "Описание эпика-1", subTask1);
-//        Epic epic2 = manager.createEpic("Эпичная задача-2", "Описание эпика-2", subTask2, subTask3);
-//
-//        System.out.println("--------------------До изменений----------------------");
-//        System.out.println(epic1.getName() + " :");
-//        epic1.getSubTaskList().forEach(System.out::println);
-//
-//        System.out.println(epic2.getName() + " :");
-//        epic2.getSubTaskList().forEach(System.out::println);
-//
-//        System.out.println("\n-----------------Изменены статусы задач---------------");
-//        manager.updateTask(epic1, Status.IN_PROGRESS);
-//        manager.updateTask(epic2, Status.IN_PROGRESS);
-//
-//        manager.getAllTasks().forEach(System.out::println);
 
         System.out.println("\n------------Созадаём задачи--------------");
         manager.createTask(new Task("Задача-1", "Описание задачи-1", Status.NEW));
